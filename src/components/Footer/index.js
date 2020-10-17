edd6ee99f85e81f392a44ab46e63a923e3371ceb
Mark <the.mark.fullton@gmail.com>
2020-10-17 16:45:41 -0700

    Added and styled footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,8 @@ import React from "react";
 
 class Footer extends React.Component {
     render() {
+        var year = new Date().getFullYear();
+
         return (
             <div className="footer">
                 <div className="footerContainer">
@@ -9,8 +11,11 @@ class Footer extends React.Component {
                         <h2>The Transformation Project</h2>
                     </div>
                     <div className="footerContentContainer">
-                        <p>Workds words words</p>
+                        
+                            <p className="youAreHere">APP</p> <p>|</p> <p><a href="https://github.com/themarkfullton/transformation-api-v2" target="_blank">API</a></p> <p>|</p> <p><a href="https://transformation-app.herokuapp.com/" target="_blank">OLD</a>
+                            </p>
                     </div>
+                    <div className="footerCopyright">Copyright &copy; {year}</div>
                 </div>
             </div>
         )
